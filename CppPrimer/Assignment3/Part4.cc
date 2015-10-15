b7d07fee4e060fc4dbf6d8bfc855a0c33eccbae8
ZSShen <andy780328@hotmail.com>
2015-10-15 10:12:20 -0700

    CppPrimer: BigNumber Arithmetic completed with addition, subtraction, multiplication, and division.

--- CppPrimer/Assignment3/Part4.cc
+++ CppPrimer/Assignment3/Part4.cc
@@ -19,6 +19,7 @@ class BigInt {
     void addNumber(const Number*, const Number*, Number*) const;
     void subNumber(const Number*, const Number*, Number*) const;
     void mulNumber(const Number*, const Number*, Number*) const;
+    void divNumber(const Number*, const Number*, Number*) const;
     bool compareLarger(const Number*, const Number*) const;
 
   public:
@@ -168,6 +169,79 @@ void BigInt::mulNumber(const Number* num_src, const Number* num_dst,
     memmove(num_res->m_array, array_res + idx_res, sizeof(char) * num_res->m_len);
 }
 
+void BigInt::divNumber(const Number* num_src, const Number* num_dst,
+                       Number* num_res) const
+{
+    // Early return if the dividend is smaller than divisor.
+    if (compareLarger(num_dst, num_src)) {
+        num_res->m_len = 1;
+        num_res->m_array = new char[1];
+        num_res->m_array[0] = 0;
+        return;
+    }
+
+    int len_divr = num_dst->m_len + 1;
+    int len_res = num_src->m_len + 1;
+    char* array_src = num_src->m_array;
+
+    char array_divr[len_divr];
+    char array_divd[len_divr];
+    char array_res[len_res];
+    array_divr[0] = 0;
+    memcpy(array_divr + 1, num_dst->m_array, len_divr - 1);
+    array_divd[0] = 0;
+    memcpy(array_divd + 1, array_src, len_divr - 1);
+    memset(array_res, 0, sizeof(char) * len_res);
+
+    int idx_res = len_divr - 1;
+    int idx_divd = num_dst->m_len - 1;
+    int iter = len_res - len_divr + 1;
+    for (int i = 0 ; i < iter ; ++i) {
+        for (int j = 0 ; j < 10 ; ++j) {
+            // Subtraction trial
+            for (int k = len_divr - 1 ; k > 0 ; --k) {
+                array_divd[k] -= array_divr[k];
+                if (array_divd[k] < 0) {
+                    array_divd[k - 1] -= 1;
+                    array_divd[k] += 10;
+                }
+            }
+            // Negative sign scanning
+            bool is_neg = false;
+            for (int k = 0 ; k < len_divr ; ++k) {
+                if (array_divd[k] < 0) {
+                    is_neg = true;
+                    break;
+                }
+            }
+
+            if (!is_neg)
+                continue;
+            // Determine the per-radix quotient and compensate the dividend.
+            array_res[idx_res++] = j;
+            for (int k = len_divr - 1 ; k > 0 ; --k) {
+                array_divd[k] += array_divr[k];
+                if (array_divd[k] > 9) {
+                    array_divd[k - 1] += 1;
+                    array_divd[k] -= 10;
+                }
+            }
+            if (idx_divd < (num_src->m_len - 1)) {
+                memmove(array_divd, array_divd + 1, len_divr);
+                array_divd[len_divr - 1] = array_src[++idx_divd];
+            }
+            break;
+        }
+    }
+
+    idx_res = 0;
+    while ((idx_res < len_res - 1) && (array_res[idx_res] == 0))
+        ++idx_res;
+    num_res->m_len = len_res - idx_res;
+    num_res->m_array = new char[num_res->m_len];
+    memmove(num_res->m_array, array_res + idx_res, sizeof(char) * num_res->m_len);
+}
+
 bool BigInt::compareLarger(const Number* num_src, const Number* num_dst) const
 {
     if (num_src->m_len > num_dst->m_len)
@@ -177,10 +251,10 @@ bool BigInt::compareLarger(const Number* num_src, const Number* num_dst) const
         return false;
 
     for (int i = 0 ; i < num_src->m_len ; ++i) {
-        if (num_src->m_array[i] < num_dst->m_array[i])
-            return false;
+        if (num_src->m_array[i] > num_dst->m_array[i])
+            return true;
     }
-    return true;
+    return false;
 }
 
 
@@ -359,7 +433,18 @@ BigInt BigInt::operator*(const BigInt& rhs) const
 }
 
 BigInt BigInt::operator/(const BigInt& rhs) const
-{}
+{
+    Number* num_res = new Number;
+
+    /* lhs and rhs have the sname sign. */
+    if (m_sign == rhs.m_sign) {
+        divNumber(m_num, rhs.m_num, num_res);
+        return BigInt(false, num_res);
+    }
+
+    divNumber(m_num, rhs.m_num, num_res);
+    return BigInt(true, num_res);
+}
 
 
 /* Overload ostream to print BigInt. */
@@ -374,48 +459,24 @@ int main()
     string str_op1, str_op2;
     char oper;
 
-
-#ifdef IN_HOUSE
-    while (cin >> str_op1) {
-        cin >> oper >> str_op2;
-        BigInt bigint_op1(str_op1.c_str());
-        BigInt bigint_op2(str_op2.c_str());
-
-        cout << "Opnd_1: " << bigint_op1 << endl;
-        cout << "Oper  : " << oper << endl;
-        cout << "Opnd_2: " << bigint_op2 << endl;
-        switch (oper) {
-            case '+':
-                cout << "Result: " << bigint_op1 + bigint_op2 << endl << endl;
-                break;
-            case '-':
-                cout << "Result: " << bigint_op1 - bigint_op2 << endl << endl;
-                break;
-            case '*':
-                cout << "Result: " << bigint_op1 * bigint_op2 << endl << endl;
-                break;
-        }
-    }
-
-#else
     cin >> str_op1 >> oper >> str_op2;
     BigInt bigint_op1(str_op1.c_str());
     BigInt bigint_op2(str_op2.c_str());
 
-    cout << bigint_op1 << endl;
-    cout << bigint_op2 << endl;
     switch (oper) {
         case '+':
-            cout << bigint_op1 + bigint_op2 << endl;
+            cout << bigint_op1 + bigint_op2;
             break;
         case '-':
-            cout << bigint_op1 - bigint_op2 << endl;
+            cout << bigint_op1 - bigint_op2;
             break;
         case '*':
-            cout << bigint_op1 * bigint_op2 << endl;
+            cout << bigint_op1 * bigint_op2;
+            break;
+        case '/':
+            cout << bigint_op1 / bigint_op2;
             break;
     }
-#endif
 
     return 0;
 }
