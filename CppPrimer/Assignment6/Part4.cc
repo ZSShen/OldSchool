9ebdc16f50917de0d8fb071e1a466e4add4747d3
ZSShen <andy780328@hotmail.com>
2015-11-03 08:34:30 -0800

    Simulate the recursive decent parsing.

--- CppPrimer/Assignment6/Part4.cc
+++ CppPrimer/Assignment6/Part4.cc
@@ -1,64 +1,245 @@
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
 #include <vector>
 
 using namespace std;
 
 
+const int SIZE_BLAH_BUF = 512;
+
+
 class StringFactory
 {
   private:
-    int m_size;
-    vector<string*> m_vec;
+    int size_;
+
+    string oper_copy;
+    string oper_add;
+    string oper_find;
+    string oper_rfind;
+    string oper_insert;
+    string oper_reset;
+    string oper_print;
+    string oper_printall;
+    string oper_over;
+
+    vector<string*> vec_;
+
+    string GetString();
+    int GetInteger();
+
+    string OperatorCopy();
+    string OperatorAdd();
+    int OperatorFind();
+    int OperatorRFind();
+    void OperatorInsert();
+    void OperatorReset();
+    void OperatorPrint();
+    void OperatorPrintAll();
 
   public:
     StringFactory(int size)
-      : m_size(size)
+      : size_(size),
+        oper_copy("copy"),
+        oper_add("add"),
+        oper_find("find"),
+        oper_rfind("rfind"),
+        oper_insert("insert"),
+        oper_reset("reset"),
+        oper_print("print"),
+        oper_printall("printall"),
+        oper_over("over"),
+        vec_(size)
     {
-        m_vec.reserve(m_size);
+        for (int i = 0 ; i < size ; ++i)
+            vec_[i] = NULL;
     }
 
     ~StringFactory()
     {
-        for (vector<string*>::iterator iter = m_vec.begin() ;
-             iter != m_vec.end() ; ++iter)
-            delete *iter;
-        m_vec.clear();
+        vector<string*>::iterator iter = vec_.begin();
+        while (iter != vec_.end()) {
+            if (*iter)
+                delete *iter;
+            ++iter;
+        }
+        vec_.clear();
     }
 
-    string copy(int idx_str, int ofst, int len);
-    string add(const string& str_fst, const string& str_snd);
-    int find(const string& sub_str, int idx_str);
-    int rfind(const string& sub_str, int idx_str);
-    int insert(const string& str, int idx_str, int ofst);
-    void reset(const string& str, int idx_str);
-    void print(int idx_str);
-    void printall();
-
-    /*
-    copy N X L：取出第N个字符串第X个字符开始的长度为L的字符串。
-
-    add S1 S2：判断S1，S2是否为0-99999之间的整数，若是则将其转化为整数做加法，若不是，则作字符串加法，返回的值为一字符串。
-
-    find S N：在第N个字符串中从左开始找寻S字符串，返回其第一次出现的位置，若没有找到，返回字符串的长度。
-
-    rfind S N：在第N个字符串中从右开始找寻S字符串，返回其第一次出现的位置，若没有找到，返回字符串的长度。
-
-    insert S N X：在第N个字符串的第X个字符位置中插入S字符串。
-
-    reset S N：将第N个字符串变为S。
-
-    print N：打印输出第N个字符串。
-
-    printall：打印输出所有字符串。
-
-    over：结束操作。
-    */
+    void Init();
+    void Run();
 };
 
+string StringFactory::GetString()
+{
+    string str;
+    cin >> str;
+
+    if (str == oper_copy)
+        return OperatorCopy();
+    if (str == oper_add)
+        return OperatorAdd();
+    return str;
+}
+
+int StringFactory::GetInteger()
+{
+    string str;
+    cin >> str;
+
+    if (str == oper_find)
+        return OperatorFind();
+    if (str == oper_rfind)
+        return OperatorRFind();
+    return atoi(str.c_str());
+}
+
+string StringFactory::OperatorCopy()
+{
+    int n = GetInteger();
+    int x = GetInteger();
+    int l = GetInteger();
+    return vec_[n - 1]->substr(x, l);
+}
+
+string StringFactory::OperatorAdd()
+{
+    string s1 = GetString();
+    string s2 = GetString();
+
+    if (s1.length() > 5)
+        return s1 + s2;
+    if (s2.length() > 5)
+        return s1 + s2;
+
+    const char* cs1 = s1.c_str();
+    for (int i = 0 ; i < s1.length() ; ++i) {
+        if (!(cs1[i] >= '0' && cs1[i] <= '9'))
+            return s1 + s2;
+    }
+    const char* cs2 = s2.c_str();
+    for (int i = 0 ; i < s2.length() ; ++i) {
+        if (!(cs2[i] >= '0' && cs2[i] <= '9'))
+            return s1 + s2;
+    }
+
+    int int1 = atoi(cs1);
+    int int2 = atoi(cs2);
+    char sum[SIZE_BLAH_BUF];
+    sprintf(sum, "%d", int1 + int2);
+    return string(sum);
+}
+
+int StringFactory::OperatorFind()
+{
+    string s = GetString();
+    int n = GetInteger();
+
+    int res = vec_[n - 1]->find(s);
+    return (res == -1)? vec_[n - 1]->length() : res;
+}
+
+int StringFactory::OperatorRFind()
+{
+    string s = GetString();
+    int n = GetInteger();
+
+    int res = vec_[n - 1]->rfind(s);
+    return (res == -1)? vec_[n - 1]->length() : res;
+}
+
+void StringFactory::OperatorInsert()
+{
+    string s = GetString();
+    int n = GetInteger();
+    int x = GetInteger();
+
+    vec_[n - 1]->insert(x, s);
+}
+
+void StringFactory::OperatorReset()
+{
+    string s = GetString();
+    int n = GetInteger();
+
+    *(vec_[n - 1]) = s;
+}
+
+void StringFactory::OperatorPrint()
+{
+    int n = GetInteger();
+    cout << *(vec_[n - 1]) << endl;
+}
+
+void StringFactory::OperatorPrintAll()
+{
+    vector<string*>::iterator iter = vec_.begin();
+    while (iter != vec_.end()) {
+        cout << **iter << endl;
+        ++iter;
+    }
+}
+
+void StringFactory::Init()
+{
+    for (int i = 0 ; i < size_ ; ++i) {
+        string in;
+        cin >> in;
+        vec_[i] = new string(in);
+    }
+}
+
+void StringFactory::Run()
+{
+    while (true) {
+        string cmd;
+        cin >> cmd;
+        if (cmd == oper_over)
+            break;
+        if (cmd == oper_copy) {
+            OperatorCopy();
+            continue;
+        }
+        if (cmd == oper_add) {
+            OperatorAdd();
+            continue;
+        }
+        if (cmd == oper_find) {
+            OperatorFind();
+            continue;
+        }
+        if (cmd == oper_rfind) {
+            OperatorRFind();
+            continue;
+        }
+        if (cmd == oper_insert) {
+            OperatorInsert();
+            continue;
+        }
+        if (cmd == oper_reset) {
+            OperatorReset();
+            continue;
+        }
+        if (cmd == oper_print) {
+            OperatorPrint();
+            continue;
+        }
+        if (cmd == oper_printall)
+            OperatorPrintAll();
+    }
+}
 
 int main()
 {
+    int size;
+    cin >> size;
+
+    StringFactory factory(size);
+    factory.Init();
+    factory.Run();
 
     return 0;
 }
